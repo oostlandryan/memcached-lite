93479cc1435bfb70a7717eab6be0e9d3331b804d
Ryan Oostland <oostlandryan16@gmail.com>
2021-10-08 00:19:45 -0400

    Finish assignment 3

--- Assignment3Report.md
+++ Assignment3Report.md
@@ -2,7 +2,13 @@
 Ryan Oostland  
 ryoost
 ## Running the Server and Client on Google Compute Engine
-A bash script, [gcpdeployment.sh](gcpdeployment.sh), starts the server and client instances in the PROJECTNAME project using gcloud. It then uses `gcloud compute ssh` to access the instances, clone the git repo containing my memcached-lite code, install Go, and finally run the programs. Memcached-lite outputs are wrapped in dashes and should be easy to read in the terminal. After the programs have ran, the server and client instances are deleted.
-## Assumptions
-The [gcpdeployment.sh](gcpdeployment.sh) script assumes that gcloud is installed and that the project is configured with Google's default network settings. It also assumes that the Memorystore API and Service Networking API are enabled for the project.
-.
+A bash script, [gcpdeployment.sh](gcpdeployment.sh), starts the server and client instances in the PROJECTNAME project using gcloud. It then uses `gcloud compute ssh` to access the instances, clone the git repo containing my memcached-lite code, install Go, and finally run the programs. Memcached-lite outputs are wrapped in dashes and should be easy to read in the terminal. After the programs have run, the server and client instances are deleted.
+## Performance
+Memcached-lite's performance was slightly better on the gcp VMs than it was locally. I was able to consitently have 1000 concurrent connections, which was did not work as well on my own machine. All other tests performed identically to how they did on my local machine.
+## Comparison with Memorystore's Memcached Service
+Unfortunately, I was unable to run memorystore's memcached service. Despite following the [online instructions](https://cloud.google.com/memorystore/docs/memcached/establishing-connection) for establishing a private services access connection, memcached failed to start citing that private service access is not enabled. I will be out of town when the assignment is due, so I'm needing to finish it early and have not left myself with enough time to solve this issue. I assumed that using Google's memcached service would be easier than setting up my own, but was clearly mistaken.
+## Costs
+A compute instance using the standard E2 machine with 2 vCPUs costs $0.067006 per hour to run.  
+Memorystore's Memcached service with 1 node located in Iowa, 1 vCPU, and 1GB would cost $0.0544 per hour.  
+Network traffic on the internal network, which my instances use to communicte with each other, is free.  
+
--- gcpdeployment.sh
+++ gcpdeployment.sh
@@ -6,12 +6,17 @@ gcloud config set project $PROJECTNAME
 # Configure Network
 gcloud compute networks create ryoost-network --subnet-mode=auto --bgp-routing-mode=regional --mtu=1460
 gcloud compute firewall-rules create ryoost-tcp --network ryoost-network --allow tcp:22,tcp:3389,icmp,tcp:9889
+sleep 5
+
+gcloud beta compute addresses create ryoost-reserved-addresses --global --prefix-length=24 --network=ryoost-network --purpose=vpc_peering
+gcloud services vpc-peerings connect --service=servicenetworking.googleapis.com --ranges=ryoost-reserved-addresses --network=ryoost-network --project=$PROJECTNAME
+
 
 # Create server and client instances
 gcloud compute instances create ryoost-server --network=ryoost-network --image-family=ubuntu-1804-lts --image-project=ubuntu-os-cloud --zone=us-central1-a
 gcloud compute instances create ryoost-client --network=ryoost-network --image-family=ubuntu-1804-lts --image-project=ubuntu-os-cloud --zone=us-central1-a
 # Wait a bit to make sure the instances are actually up and running
-sleep 5
+sleep 10
 
 # Make sure ssh keys are updated (sometimes this fixes issues, sometimes it does not)
 gcloud compute config-ssh
@@ -27,22 +32,23 @@ cd memcached-lite
 yes | sudo apt install golang-go
 go run server.go -port=9889" &
 # Since the previous command is run in the background, we don't know if the server is ready when the next command runs, so we'll wait a bit
-sleep 5
+sleep 10
 
 # # ssh into client instance, clone memcached-lite repo, move to repo, install Go, run client
 gcloud compute ssh ryoost-client --zone=us-central1-a --command="git clone git://github.com/oostlandryan/memcached-lite.git
 cd memcached-lite
 yes | sudo apt install golang-go
-go run client.go -server=$SERVERADDRESS:9889
-exit"
+go run client.go -server=$SERVERADDRESS:9889"
 
 # Test Google's Memorystore
-#gcloud memcache instances create ryoost-memcache --node-count=1 --node-cpu=1 --node-memory=1GB --region=us-central1
+gcloud memcache instances create ryoost-memcache --node-count=1 --node-cpu=1 --node-memory=1GB --region=us-central1
 
 # Delete server and client instances
 yes | gcloud compute instances delete ryoost-server
 yes | gcloud compute instances delete ryoost-client
-#yes | gcloud memcache instances delete ryoost-memcache
+yes | gcloud memcache instances delete ryoost-memcache --region=us-central1
 yes | gcloud compute firewall-rules delete ryoost-tcp
+yes | gcloud services vpc-peerings delete --service=servicenetworking.googleapis.com --network=ryoost-network
+yes | gcloud beta compute addresses delete ryoost-reserved-addresses --global
 sleep 5
 yes | gcloud compute networks delete ryoost-network
